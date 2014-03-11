c9b468cb479377ac026a0e6f9dd05df91a8d4869
JmAbuDabi <dambldor91@gmail.com>
2014-03-12 00:43:57 +0200

    working

---
+++ vktest/vkPHP/temp/script.js
@@ -0,0 +1,19 @@
+function VKAutorized()
+{
+	 VK.init(function()
+	 { 
+		apiId: 4237766
+		// Your code here 
+		alert("sucses");
+	 },
+	 function()
+	 { 		
+		alert("failed");
+	 }, '5.14');
+}
+
+
+function getWallMessage()
+{
+	
+}
